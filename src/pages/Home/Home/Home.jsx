78802b55bdba62e910e8bcca531dee81241919f6
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-01-10 12:51:22 +0600

    Complete : left section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import { MdOutlineNoteAlt } from "react-icons/md";
 import note from "../../../assets/image/note.jpg";
 import user from "../../../assets/image/user.jpg";
+import { LuFileInput } from "react-icons/lu";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div className="container mx-auto pt-5">
+    <div className="container mx-auto py-5">
       {/* Left Side section  */}
       <section className="w-1/4 bg-indigo-50 rounded-xl border-2">
-        <div>
+        <div className="">
           <img src={note} alt="" className="p-7" />
           <h2 className="text-center text-2xl text-indigo-400 font-bold">
             Your Result!
@@ -71,13 +73,43 @@ const Home = () => {
 
             {/* third div */}
 
-              <div className="py-2">
-                <h2 className="font-bold">Improve your Marks</h2>
-                <p className="text-xs text-gray-400 font-bold">Improve Your Score By practice more.</p>
-              
-              <button type="button " className="bg-indigo-500 text-white w-full rounded mt-8 p-1">Practice more</button>
-              </div>
+            <div className="py-2">
+              <h2 className="font-bold">Improve your Marks</h2>
+              <p className="text-xs text-gray-400 font-bold">
+                Improve Your Score By practice more.
+              </p>
 
+              <button
+                type="button "
+                className="bg-indigo-500 text-white w-full rounded mt-8  p-1"
+              >
+                Practice more
+              </button>
+            </div>
+          </div>
+
+          {/* left section secund div */}
+
+          <div className="p-5 m-5 border-2 bg-white rounded-xl">
+            <h2 className="font-bold">Revisit Paper</h2>
+            <p className="text-gray-600 text-sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            </p>
+            <div className="">
+              <button
+                type="button"
+                className="bg-indigo-500 text-white rounded p-1 my-5 w-5/6 flex items-center justify-center gap-2 mx-auto"
+              >
+                <LuFileInput /> <span>Visit</span>
+              </button>
+            </div>
+            <div className="flex gap-1">
+              <AiOutlineExclamationCircle className="text-gray-400" />
+              <p className="text-xs text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Corporis sapiente magnam quae{" "}
+              </p>
+            </div>
           </div>
         </div>
       </section>
