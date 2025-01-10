3e5255c6247e69a3de5b24ce1f311791c639b32f
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2025-01-10 12:11:02 +0600

    Add : left section 2nd div

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { MdOutlineNoteAlt } from "react-icons/md";
 import note from "../../../assets/image/note.jpg";
+import user from "../../../assets/image/user.jpg";
 
 const Home = () => {
   return (
@@ -19,7 +20,7 @@ const Home = () => {
 
           <div className="p-5 m-5 border-2 bg-white rounded-xl">
             {/* first div */}
-            <div className="bg-indigo-50 rounded-lg flex">
+            <div className="bg-indigo-50 rounded-lg flex mb-4">
               <div className="p-2">
                 <MdOutlineNoteAlt className="bg-indigo-200 text-4xl p-2 rounded-lg" />
               </div>
@@ -34,36 +35,37 @@ const Home = () => {
               </div>
 
               <div className="ml-auto p-2 rounded-lg flex items-center justify-center">
-                <div className="border-l-2 border-gray-300 h-16 mx-4"></div>{" "}
-                <div className="text-center">
-                  <p className="text-white bg-green-600 p-1 w-fit rounded mx-auto">{`76%`}</p>
-                  <p className="text-green-600">ACCURACY</p>
+                <div className="border-l-2 border-gray-300 h-9 mx-4"></div>{" "}
+                <div className="text-center ">
+                  <p className="text-white bg-green-600 p-0.5 w-fit text-xs rounded mx-auto">{`76%`}</p>
+                  <p className="text-green-600 text-xs">ACCURACY</p>
                 </div>
               </div>
             </div>
 
             {/* second div */}
-            <div className="bg-indigo-50 rounded-lg ">
+
+            <div className="bg-indigo-50 rounded-lg py-2">
               <div className="flex">
                 <div className="p-2">
-                  <MdOutlineNoteAlt className="bg-indigo-200 text-4xl p-2 rounded-lg" />
+                  <img src={user} alt="" className="w-9 h-9" />
                 </div>
                 <div className="p-2">
-                  <p className=" rounded-lg text-xs px-1 text-gray-500">
-                    Top Scored
+                  <p className="rounded-lg text-xs px-1 text-black">
+                    Top Score
                   </p>
                   <div className="flex items-end">
-                    <p className="font-bold text-xl leading-none">230</p>
+                    <p className="font-bold text-xl leading-none">136</p>
                     <p className="text-xs leading-3 ml-1 self-end">/240</p>
                   </div>
                 </div>
               </div>
-              <hr className="mx-1 p-2 " />
-              <div className="px-2  flex ">
-                <p className="text-gray-400 text-sm">
+              <hr className="mx-5 py-2 " />
+              <div className="flex justify-between px-5">
+                <p className="text-center text-xs text-gray-500 font-bold">
                   By <span className="text-black">Parth Akotkar</span>
                 </p>
-                <p className="text-white bg-green-600 p-1 w-fit rounded mx-auto">{`92% ACCURACY`}</p>
+                <p className="text-white bg-green-600 px-1 w-fit text-xs rounded-lg mx-auto ">{`92% ACCURACY`}</p>
               </div>
             </div>
           </div>
